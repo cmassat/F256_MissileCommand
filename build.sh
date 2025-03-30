9d9aea37bf5839a4083469fe31caa2006a1cd7d1
bill.massat <bill.massat@outlook.com>
2025-03-29 22:52:29 -0400

    Added git lfs

--- build.sh
+++
@@ -1,4 +0,0 @@
-64tass  --no-warn --map "main.map" --output-exec=start --c256-pgz "./src/main.asm" --m65816  --list="app.lst" -Wno-portable -o "app.pgz"
-export FOENIXMGR='/mnt/d/Retro/Foenix/Repos/FoenixMgr'
-sudo chmod 666 /dev/ttyUSB0
-python $FOENIXMGR/FoenixMgr/fnxmgr.py --copy app.pgz
